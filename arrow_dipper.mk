a128d03364b720c50c900a2c47bb8ad1126bb810
tissotsaksham <briantissot72@gmail.com>
2018-09-13 12:30:19 +0200

    Arrow-Ify

--- arrow_dipper.mk
+++ arrow_dipper.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
+PRODUCT_NAME := arrow_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 8
